4e43f3b15ed5942da8d085077dde719aa0f30966
Woodbin <woodbin@centrum.cz>
2014-09-24 00:22:41 +0200

    Debug module work
    basic output method

--- src/DebugModule.java
+++ src/DebugModule.java
@@ -3,11 +3,42 @@
  */
 public class DebugModule {
     private static DebugModule ourInstance = new DebugModule();
-
+    private static  boolean consoleWindowExists;
     public static DebugModule getInstance() {
         return ourInstance;
     }
 
     private DebugModule() {
+        consoleWindowExists=false;
     }
+
+    public static void debugOut(String message){
+        processOut(message);
+    }
+
+    public static void debugIn(String command){
+     processIn(command);
+    }
+
+    public static void consoleWindowState(boolean state){
+        consoleWindowExists = state;
+    }
+
+
+
+    private static void processOut(String message){
+        if(consoleWindowExists){
+            //TODO console window output
+        }
+        System.out.println(message);
+    }
+
+    private static void processIn(String command){
+
+    }
+
+
+
+
+
 }
