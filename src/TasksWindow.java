e1fbb3ce538409f2edfb128eb64f8c1dc938c65b
Woodbin <woodbin@centrum.cz>
2014-09-24 22:39:18 +0200

    TasksWindow created
    Created TasksWindow

---
+++ src/TasksWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Woodbin on 24.9.2014.
+ */
+public class TasksWindow {
+    private JList tasksList;
+    private JPanel tasksPanel;
+    private JButton startTaskButton;
+    private JButton postponeTaskButton;
+    private JButton refreshButton;
+}
